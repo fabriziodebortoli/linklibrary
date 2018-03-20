425a6466d229941da892c4cfe6b0b2b1a8caab43
Fabrizio De Bortoli <fabrizio.debortoli@microarea.it>
2018-03-20 12:11:25 +0100

    new component in folder

---
+++ linklibrary/src/folder/sample2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sample2-component',
+    template: `<h1>Sample 2 componentYYY</h1>`
+})
+export class Sample2Component {
+
+    constructor() {
+    }
+
+}
--- linklibrary/src/index.ts
+++ linklibrary/src/index.ts
@@ -1,14 +1,17 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SampleComponent } from './sample.component';
+import { Sample2Component } from './folder/sample2.component';
 import { SampleDirective } from './sample.directive';
 import { SamplePipe } from './sample.pipe';
 import { SampleService } from './sample.service';
 
 export * from './sample.component';
+export * from './folder/sample2.component';
 export * from './sample.directive';
 export * from './sample.pipe';
 export * from './sample.service';
+
 
 @NgModule({
   imports: [
@@ -16,11 +19,13 @@ export * from './sample.service';
   ],
   declarations: [
     SampleComponent,
+    Sample2Component,
     SampleDirective,
     SamplePipe
   ],
   exports: [
     SampleComponent,
+    Sample2Component,
     SampleDirective,
     SamplePipe
   ]
